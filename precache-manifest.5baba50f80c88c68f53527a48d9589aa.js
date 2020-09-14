47c9bcd63d90fcde2fd1a42d1e8e5e0ce62f1b11
Nahbuma Gana <nfg23@cornell.edu>
2020-09-14 10:34:56 -0400

    Updates

---
+++ precache-manifest.5baba50f80c88c68f53527a48d9589aa.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32fff0355cffd5cfab645b9985bcf05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "749de6df454525ddc816",
+    "url": "/static/css/main.1bdbec5f.chunk.css"
+  },
+  {
+    "revision": "28b31e0b8a551d3ab5ee",
+    "url": "/static/js/2.fd0b720f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd0b720f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749de6df454525ddc816",
+    "url": "/static/js/main.2b490555.chunk.js"
+  },
+  {
+    "revision": "d74645dde6d9d204632b",
+    "url": "/static/js/runtime-main.056f0aac.js"
+  },
+  {
+    "revision": "6cfc350084372090228fc8630b3c7eb1",
+    "url": "/static/media/Nunito-Bold.6cfc3500.ttf"
+  },
+  {
+    "revision": "6198e4056babec12dec402d68fb002f7",
+    "url": "/static/media/Nunito-Regular.6198e405.ttf"
+  },
+  {
+    "revision": "ce52b0b05414be31cdfe683b70a9ca97",
+    "url": "/static/media/Nunito-SemiBold.ce52b0b0.ttf"
+  },
+  {
+    "revision": "31ffec15d9c1b21ff3839e41fc6dfeb9",
+    "url": "/static/media/aid.31ffec15.svg"
+  },
+  {
+    "revision": "7f5c2fd20e7f09fdafec95eb256d0b9b",
+    "url": "/static/media/code.7f5c2fd2.svg"
+  },
+  {
+    "revision": "f62b8b4a15547bf185a6187dc1b87ccb",
+    "url": "/static/media/email.f62b8b4a.svg"
+  },
+  {
+    "revision": "85e634860aa7a21731582f15b0298d12",
+    "url": "/static/media/emailf.85e63486.svg"
+  },
+  {
+    "revision": "417e0772b8e247276e78635ba0bf5f50",
+    "url": "/static/media/emailmock.417e0772.svg"
+  },
+  {
+    "revision": "1778a85cde0faa6fdab8214303a9d935",
+    "url": "/static/media/emojis.1778a85c.png"
+  },
+  {
+    "revision": "fad04860148daf64d0f251dee2e69b45",
+    "url": "/static/media/fb.fad04860.svg"
+  },
+  {
+    "revision": "c1afdef5cd454d2643e087c0b95e6582",
+    "url": "/static/media/fbf.c1afdef5.svg"
+  },
+  {
+    "revision": "1a83796fefe0256ee0bc676debcd7fca",
+    "url": "/static/media/footer.1a83796f.png"
+  },
+  {
+    "revision": "4e5aaed1df334ea4b7a0eb5debd77f18",
+    "url": "/static/media/glogo.4e5aaed1.svg"
+  },
+  {
+    "revision": "7a5a48708807af67349f1c6cd660360b",
+    "url": "/static/media/gradient1.7a5a4870.png"
+  },
+  {
+    "revision": "24be584ac12e35393be40310f1f24e79",
+    "url": "/static/media/hand.24be584a.png"
+  },
+  {
+    "revision": "8768d97d6c67d57519577ab57f256539",
+    "url": "/static/media/header.8768d97d.png"
+  },
+  {
+    "revision": "4ef54fa5a78b36d9de55b5990ff29730",
+    "url": "/static/media/insta.4ef54fa5.svg"
+  },
+  {
+    "revision": "e6c57755c13beda45ac3f3375fab0258",
+    "url": "/static/media/instaf.e6c57755.svg"
+  },
+  {
+    "revision": "156988fae755600c09a52d245f33dbca",
+    "url": "/static/media/logo.156988fa.svg"
+  },
+  {
+    "revision": "7becf4cd7149a4f1c78afc331edbcae7",
+    "url": "/static/media/manrun.7becf4cd.svg"
+  },
+  {
+    "revision": "21b3fe73e389b9fc7ca9fd07a364e75e",
+    "url": "/static/media/message.21b3fe73.png"
+  },
+  {
+    "revision": "0a54167f2cec00ef9d23dbc2c3fa974e",
+    "url": "/static/media/phone.0a54167f.png"
+  },
+  {
+    "revision": "d267e85aedff0bb729b0728145b81917",
+    "url": "/static/media/qrcode.d267e85a.svg"
+  },
+  {
+    "revision": "a025e81cbb2baf3cb3c1fe2570b2b22f",
+    "url": "/static/media/store.a025e81c.svg"
+  },
+  {
+    "revision": "6a58952d6babfa075714e6a372db54ee",
+    "url": "/static/media/tower.6a58952d.svg"
+  },
+  {
+    "revision": "fcdb5197e5a3a9d69ea8009a709be67e",
+    "url": "/static/media/watch.fcdb5197.svg"
+  }
+]);
